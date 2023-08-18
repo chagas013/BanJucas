a835dd9b092e6a91415b2c0a43b5d026217ce040
CAUA SILVA CHAGAS <caua.schagas@senacsp.edu.br>
2023-08-17 22:20:17 -0300

    Connection String

--- PrjtAula01/Program.cs
+++ PrjtAula01/Program.cs
@@ -11,7 +11,7 @@ namespace PrjtAula01
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Collection());
+            Application.Run(new TelaLogin());
             
         }
     }
